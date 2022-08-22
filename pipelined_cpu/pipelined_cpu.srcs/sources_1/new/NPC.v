4eb004ce8ed36ad22bc02881be37df396d725160
王木吟 <wmy-zz@163.com>
2022-08-22 22:34:13 +0800

    add NPC

--- pipelined_cpu/pipelined_cpu.srcs/sources_1/new/NPC.v
+++ pipelined_cpu/pipelined_cpu.srcs/sources_1/new/NPC.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include"header.h"
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -21,6 +22,20 @@
 
 
 module NPC(
-
+    input [31:0]i_addr,
+    input [1:0] j_ctrl,
+    input [25:0] instr_part,
+    input [31:0] reg_data,
+    output  wire [31:0]pc
+    
     );
+    
+    wire [25:0]instr_index = instr_part;
+    wire [15:0]offset = instr_part[15:0];
+    
+    assign pc = (j_ctrl == `JUMP_J)? {i_addr[31:28], instr_index, 2'b00}:
+                (j_ctrl == `JUMP_BEQ)? i_addr + (offset<<2):
+                (j_ctrl == `JUMP_REG)? reg_data:
+                i_addr+4;
+    
 endmodule
