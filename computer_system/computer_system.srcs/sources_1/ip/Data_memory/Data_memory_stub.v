3b1e93e1a2e1cccf2dbbd1a6679ea0de5a96e1a5
habanera <ffflfffl@126.com>
2022-08-28 20:35:25 +0800

    新建项目并忽略所有srcs目录以外的内容

---
+++ computer_system/computer_system.srcs/sources_1/ip/Data_memory/Data_memory_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Sun Aug 28 20:22:33 2022
+// Host        : LAPTOP-FNQMD2JI running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               E:/repos/hardware-labs/computer_system/computer_system.srcs/sources_1/ip/Data_memory/Data_memory_stub.v
+// Design      : Data_memory
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "dist_mem_gen_v8_0_13,Vivado 2019.2" *)
+module Data_memory(a, d, clk, we, spo)
+/* synthesis syn_black_box black_box_pad_pin="a[9:0],d[31:0],clk,we,spo[31:0]" */;
+  input [9:0]a;
+  input [31:0]d;
+  input clk;
+  input we;
+  output [31:0]spo;
+endmodule
---
+++ computer_system/computer_system.srcs/sources_1/new/ram.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module RAM(
+    input wire clk,
+    input wire en_wt_mem,
+    input wire[31:0] addr,
+    input wire[31:0] write_data,
+
+    output wire[31:0] data_out
+    );
+
+Data_memory dm(
+    .a(addr[11:2]),
+    .d(write_data),
+    .clk(clk),
+    .we(en_wt_mem),
+    .spo(data_out)
+);
+
+endmodule
